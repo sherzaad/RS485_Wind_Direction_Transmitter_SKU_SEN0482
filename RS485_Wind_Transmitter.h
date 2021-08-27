da6739f46a90d06e73cb905aa09968cc1134c5e2
Sherzaad <57634578+sherzaad@users.noreply.github.com>
2021-08-27 06:37:29 +0100

    Update RS485_Wind_Transmitter.h

--- RS485_Wind_Transmitter.h
+++ RS485_Wind_Transmitter.h
@@ -1,1 +1,28 @@
+#ifndef RS485_Wind_Transmitter_h_
+#define RS485_Wind_Transmitter_h_
+
+#include "Arduino.h"
+
+class RS485_Wind_Transmitter
+{
+  public:
+    RS485_Wind_Transmitter() {}
+    void addedCRC(unsigned char *buf, int len);
+    unsigned int CRC16_2(unsigned char *buf, int len);
+    float readWindSpeed(unsigned char Address);
+    int readWindDirection(unsigned char Address);
+    unsigned char ModifyAddress(unsigned char Address1, unsigned char Address2);
+    ~RS485_Wind_Transmitter() {}
+
+    inline void Init(HardwareSerial *s) {
+      ser = s;
+      ser->begin(9600);
+    }
+
+  private:
+    HardwareSerial *ser;
+
+};
+
+#endif
 
